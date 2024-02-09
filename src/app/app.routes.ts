2cb6cb36ea2177999ac33a7792b68947b78f6b24
MyroslavSB <Myroslavsobchyshyn@gmail.com>
2024-02-09 22:07:00 +0100

    landing in progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./pages/landing-page/landing-page.component').then(mod => mod.LandingPageComponent)
+  }
+];
