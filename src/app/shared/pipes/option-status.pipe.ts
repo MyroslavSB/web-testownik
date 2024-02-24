d65f13e6a8668b3ad5bf578a4b9fea7d780ac8c9
MyroslavSB <Myroslavsobchyshyn@gmail.com>
2024-02-24 23:45:32 +0100

    statuses in progress

---
+++ src/app/shared/pipes/option-status.pipe.ts
@@ -0,0 +1,33 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {EOptionStatuses} from "../enums/e-option-statuses";
+
+@Pipe({
+  name: 'optionStatus',
+  standalone: true
+})
+export class OptionStatusPipe implements PipeTransform {
+  private optionStatuses = EOptionStatuses
+
+  transform(
+    option_id: number,
+    picked_options: number[],
+    correct_options: number[],
+    show_statuses: boolean = false
+  ): EOptionStatuses[] {
+    const pickedStatuses = [this.optionStatuses.PICKED_ANSWER]
+
+    if (!show_statuses) {
+      return picked_options.includes()
+    }
+
+    const isOptionPicked = picked_options.includes(option_id);
+    const isOptionCorrect = correct_options.includes(option_id);
+
+    if (isOptionPicked) {
+      return isOptionCorrect ? this.optionStatuses.CORRECT_ANSWER : this.optionStatuses.WRONG_ANSWER;
+    }
+
+    return isOptionCorrect ? this.optionStatuses.MISSED_ANSWER : null;
+
+  }
+}
