e41a6ab16d85131da5b7a03b77577218e840d41a
MyroslavSB <Myroslavsobchyshyn@gmail.com>
2024-02-25 13:06:48 +0100

    refactored file parsing

--- src/app/shared/strategies/txt-file-parsing-strategy.ts
+++ src/app/shared/strategies/txt-file-parsing-strategy.ts
@@ -10,37 +10,7 @@ export class TxtFileParsingStrategy implements IFileParsingStrategy {
 
       reader.onload = () => {
         const content: string = reader.result as string;
-        const lines: string[] = content.split(/\r?\n/);
-
-        const correct_option_ids: number[] = [];
-
-        let index = -1;
-        while ((index = lines[0].indexOf('1', index + 1)) !== -1) {
-          correct_option_ids.push(index - 1);
-        }
-
-        const question: string = lines[1].trim();
-
-        let fe_id: number = 0;
-        const options: IQuestionOption[] = lines.slice(2).map((line: string): IQuestionOption => {
-            return {
-              text: line.trim(),
-              id: fe_id++
-            }
-          }
-        )
-
-        const file_name: string = file.name;
-
-        const questionObj: IQuestion = {
-          question,
-          fe_id,
-          remaining_attempts: 2,
-          options,
-          correct_option_ids,
-          file_name
-        }
-
+        const questionObj = this.processContent(content, fe_id, file.name);
         observer.next(questionObj);
         observer.complete();
       };
@@ -50,4 +20,33 @@ export class TxtFileParsingStrategy implements IFileParsingStrategy {
       reader.readAsText(file);
     });
   }
-}
+
+  processContent(content: string, fe_id: number, file_name: string): IQuestion {
+    const lines: string[] = content.split(/\r?\n/);
+
+    const correct_option_ids: number[] = [];
+    let index = -1;
+    while ((index = lines[0].indexOf('1', index + 1)) !== -1) {
+      correct_option_ids.push(index);
+    }
+
+    const question: string = lines[1].trim();
+
+    let option_id: number = 1;
+    const options: IQuestionOption[] = lines.slice(2).map((line: string): IQuestionOption => {
+        return {
+          text: line.trim(),
+          id: option_id++
+        }
+      }
+    );
+
+    return {
+      question,
+      fe_id,
+      remaining_attempts: 2,
+      options,
+      correct_option_ids,
+      file_name
+    };
+  }}
